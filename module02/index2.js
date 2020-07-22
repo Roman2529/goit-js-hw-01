265a6541f0a67a7b95861fea15dabb7da7ee897a
Roman Demikhov <demikhovroman@gmail.com>
2020-07-22 09:03:59 +0300

    6

--- module02/index2.js
+++ module02/index2.js
@@ -1,1 +1,125 @@
 "use strict";
+
+//--------------------01
+
+// const logItems = function (array) {
+//     let index = 1;
+//     for (const item of array) {
+//         console.log(`${index} - ${item}`)
+//         index += 1;
+//     }
+// }
+// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+//--------------------02
+
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//     return message.split(" ").length * pricePerWord;
+// }
+
+// console.log(
+//     calculateEngravingPrice(
+//         'Proin sociis natoque et magnis parturient montes mus',
+//         10,
+//     ),
+// ); // 80
+
+// console.log(
+//     calculateEngravingPrice(
+//         'Proin sociis natoque et magnis parturient montes mus',
+//         20,
+//     ),
+// ); // 160
+
+// console.log(
+//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
+
+//-----------------03
+
+// const findLongestWord = function (string) {
+//   let words = string.split(" ");
+//   let max = 0;
+//   let longestWord;
+
+//   for (const word of words) {
+//     let len = word.length;
+//     if (len > max) {
+//       max = len;
+//       longestWord = word;
+//     }
+//   }
+
+//   return longestWord;
+// };
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+// console.log(findLongestWord("Google do a roll")); // 'Google'
+// console.log(findLongestWord("May the force be with you")); // 'force'
+
+//--------------------04
+
+// const formatString = string => string.length > 40 ? `${string.slice(0, 41)}...` : string;
+
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // вернется оригинальная строка
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // вернется форматированная строка
+
+// console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
+
+// console.log(
+//     formatString(
+//         'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+//     ),
+// );
+// // вернется форматированная строка
+
+//------------------------05
+
+// const checkForSpam = function (message) {
+//     let messageForCheck = message.toLowerCase();
+//     return messageForCheck.includes("spam") || messageForCheck.includes("sale");
+// };
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+//------------------------06
+
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//     input = prompt("Введите номер:");
+//     numbers.push(Number(input));
+// } while (input !== null);
+
+// if (numbers.length > 0) {
+//     for (const number of numbers) {
+//         total += number;
+//     }   
+//     console.log(`Общая сумма чисел равна ${total}`);
+// }
+
+//------------------------07
+
+
+
+
+
+
+
+
