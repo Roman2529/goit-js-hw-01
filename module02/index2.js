ea39d3ae354c0aecfa0fbde86ef358f3a371ff80
Roman Demikhov <demikhovroman@gmail.com>
2020-07-23 08:13:52 +0300

    22

--- module02/index2.js
+++ module02/index2.js
@@ -1,9 +1,9 @@
 "use strict";
 
 //--------------------01
-
+// let index = 1;
 // const logItems = function (array) {
-//     let index = 1;
+//     
 //     for (const item of array) {
 //         console.log(`${index} - ${item}`)
 //         index += 1;
@@ -116,10 +116,33 @@
 
 //------------------------07
 
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
+// const isLoginValid = function (login) {
+//      let loginLength = login.length;
+//     return loginLength >= 4 && loginLength <= 16;
+//     }
 
+// function isLoginUnique(allLogins, login) {
+//     return !allLogins.includes(login);
+// }
 
-
-
+// function addLogin(allLogins, login) {
+//     if (!isLoginValid(login)) {
+//         return "Ошибка! Логин должен быть от 4 до 16 символов";
+//     } else if (!isLoginUnique(allLogins, login)) {
+//         return "Такой логин уже используется!";
+//     } else {
+//         logins.push(login);
+//         return "Логин успешно добавлен!";
+//     }
+// }
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
 
