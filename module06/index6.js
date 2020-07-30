349d0b495c6d91445dfe5dbf14473fa1832d0753
Roman Demikhov <demikhovroman@gmail.com>
2020-07-30 21:30:14 +0300

    1

--- module06/index6.js
+++ module06/index6.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log(this);
+import users from "./users.js";
+//----------------01
+
+const getUserNames = users => {
+    return users.map(user => user.name);
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+    
+
+
+
+
